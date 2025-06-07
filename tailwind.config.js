11b3e4236f8e399f4f4a9137d053c479d4ca6722
Maxim Jackson <maxim.jackson@live.com>
2025-06-07 15:04:30 -0700

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+﻿/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './my_portfolio_app/templates/**/*.html',
+    './my_portfolio_app/**/*.py',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
