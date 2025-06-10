1e066bd626650000e19ddd043a93c52320a92643
Maxim Jackson <maxim.jackson@live.com>
2025-06-09 17:38:00 -0700

    tailwindcss + Django is a pain

--- my_portfolio/settings.py
+++ my_portfolio/settings.py
@@ -16,7 +16,6 @@ import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-STATIC_URL = '/static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'my_portfolio_app/static')] 
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 
