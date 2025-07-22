b36139ebfe7c375625fb9b9090513d14ef170060
Maxim Jackson <jammaxim@amazon.com>
2025-07-22 08:42:00 -0700

    chore: attempted Tailwind CSS integration with Django staticfiles (not working, needs troubleshooting)

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,10 @@ module.exports = {
   content: [
     './my_portfolio_app/templates/**/*.html',
     './my_portfolio_app/**/*.py',
+    './my_portfolio_app/static/**/*.js',
+    './my_portfolio_app/static/**/*.html',
+    './my_portfolio_app/templates/*.html',
+    './*.html'
   ],
   theme: {
     extend: {},
